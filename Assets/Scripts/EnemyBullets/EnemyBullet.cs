c554365151bfb8c87eef8aef1e01a58a743425ba
dhiraj7894 <dhiraj7894@gmail.com>
2020-06-08 13:17:34 +0530

    dd

---
+++ Assets/Scripts/EnemyBullets/EnemyBullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (EnemyController.enemy.hitMuzzel[0] != null)
+        {
+            var hitVFX = Instantiate(EnemyController.enemy.hitMuzzel[0], transform.position, Quaternion.identity);
+            Destroy(hitVFX, 0.5f);
+        }
+        Destroy(gameObject, 0.1f);
+    }
+}
