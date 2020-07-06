191b3a9c9da5a492e4116d7770da38de113f157f
dhiraj7894 <dhiraj7894@gmail.com>
2020-07-06 13:22:43 +0530

    work

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -10,11 +10,26 @@ public class Rotator : MonoBehaviour
      float angle1;
      float angle2;
      float angle3;
-    void Update()
+    void FixedUpdate()
     {
-        angle1 += Time.deltaTime * speed1;
-        angle2 += Time.deltaTime * speed2;
-        angle3 += Time.deltaTime * speed3;
-        transform.localRotation = Quaternion.Euler(angle1,angle2, angle3);
+        if (speed2 == 0 && speed3 == 0)
+        {
+            angle1 += Time.deltaTime * speed1;
+            transform.localRotation = Quaternion.Euler(angle1, transform.localRotation.y, transform.localRotation.z);
+        }
+
+        if (speed1 == 0 && speed3 == 0)
+        {
+            angle2 += Time.deltaTime * speed2;
+            transform.localRotation = Quaternion.Euler(transform.localRotation.x, angle2, transform.localRotation.z);
+        }
+
+        if (speed1 == 0 && speed2==0)
+        {
+            angle3 += Time.deltaTime * speed3;
+            transform.localRotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.y, angle3);
+        }
+        
+        
     }
 }
--- Assets/test_1.cs
+++ Assets/test_1.cs
@@ -5,12 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class test_1 : MonoBehaviour
 {
-
+    public Animator anime;
+    public GameObject magicCircle;
+    public GameObject magicSword;
+    public Vector3[] pos;
+    public GameObject[] gameOb;
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
     void Update()
     {
+        Application.targetFrameRate = 60;
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(return_1());
+            StartCoroutine(fire());
+            StartCoroutine(fire2());
+            for(int i = 0; i < gameOb.Length; i++)
+            {
+                gameOb[i].SetActive(false);
+            }
+        }
+
+    }
+    IEnumerator return_1(){
+        anime.SetBool("fire", true);
+        yield return new WaitForSeconds(2.5f);
+        anime.SetBool("fire", false);
+    }
+    IEnumerator fire()
+    {
+        yield return new WaitForSeconds(1f);
+        //Destroy(Instantiate(magicCircle, new Vector3(0.077f, 0.7f, 0.42f), Quaternion.identity), 10);
+        Destroy(Instantiate(magicCircle, pos[0], Quaternion.identity), 10);
+    }
+    IEnumerator fire2()
+    {
+        yield return new WaitForSeconds(1.5f);
+        GameObject sword = Instantiate(magicSword, pos[1], Quaternion.identity);
+        Rigidbody rb = sword.GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.forward * 2, ForceMode.Impulse);
+        Destroy(sword, 8);
     }
 }
