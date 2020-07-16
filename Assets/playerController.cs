8625e8b1caec69fb9bc4fc1db66e8f6544cfbe5f
dhiraj7894 <dhiraj7894@gmail.com>
2020-07-16 14:54:50 +0530

    ss

---
+++ Assets/playerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Animator anime;
+    public float speed=5;
+    public float speedR=40;
+
+    float x, y;
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        anime.SetFloat("vertical",y);
+        anime.SetFloat("horizontal",x);
+        movement();
+    }
+    void movement()
+    {
+        transform.position += (Vector3.forward * speed) * y * Time.deltaTime;
+
+        transform.localEulerAngles += new Vector3(0,x * speedR * Time.deltaTime,0);
+    }
+}
